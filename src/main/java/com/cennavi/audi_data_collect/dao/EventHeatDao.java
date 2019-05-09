a3d12d466facfb9b4023c3528ec5388a9bd6f196
cennavi-mwk <moweikang@cennavi.com.cn>
2019-05-09 14:35:33 +0800

    增加聚合图接口

--- src/main/java/com/cennavi/audi_data_collect/dao/EventHeatDao.java
+++ src/main/java/com/cennavi/audi_data_collect/dao/EventHeatDao.java
@@ -3,6 +3,8 @@ package com.cennavi.audi_data_collect.dao;
 import com.cennavi.audi_data_collect.util.CommUtils;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.WKTReader;
+
+import net.sf.json.JSONArray;
 import no.ecc.vectortile.VectorTileEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -46,7 +48,52 @@ public class EventHeatDao {
     }
     
     public Map<String, Object> eventAggregateFigure(Map<String, Object> map) throws Exception {
-    	String sql = "select * from guangdong_traffic_info ";
+    	String isContinuous = map.get("isContinuous").toString();
+    	String sql = "";
+    	
+    	sql = "select e.type_code typeId,e.type_name typeName,c.geom,st_astext(c.geom) as wkt from collection_info_new c " + 
+				"left join gaosu_segment s on s.id=c.segment_id  " + 
+    			"left join event_type e on c.event_type=e.type_code " + 
+				"where 1=1 ";
+		if(map.get("city") != null) {
+			sql += " and c.city_name='"+map.get("city")+"'";
+		}
+		if(map.get("dateList") != null) {
+			JSONArray dateList = (JSONArray)map.get("dateList");
+			if(isContinuous.equals("true")) {
+				sql += " and substring(c.upload_time,0,11) between '"+dateList.get(0)+"' and '"+dateList.get(1)+"'";
+	    	}else {
+	    		String es2 = "(";
+				for(int i=0;i<dateList.size();i++) {
+					es2 += "'" + dateList.get(i) + "',";
+				}
+				es2 = es2.substring(0, es2.length()-1) + ")";
+				sql += " and substring(c.upload_time,0,11) in "+es2;
+	    	}
+		}
+		if(map.get("eventsList") != null) {
+			JSONArray eventsList = (JSONArray)map.get("eventsList");
+			String es = "(";
+			for(int i=0;i<eventsList.size();i++) {
+				es += "'" + eventsList.get(i) + "',";
+			}
+			es = es.substring(0, es.length()-1) + ")";
+			sql += " and c.event_type in " + es;
+		}
+		if(map.get("timeFrame") != null) {
+			JSONArray timeFrame = (JSONArray)map.get("timeFrame");
+			sql += " and substring(c.upload_time,12,5) between '"+timeFrame.get(0)+"' and '"+timeFrame.get(1)+"'";
+		}
+		if(map.get("roadSecList") != null) {
+			JSONArray roadSecList = (JSONArray)map.get("roadSecList");
+			String es1 = "(";
+			for(int i=0;i<roadSecList.size();i++) {
+				es1 += roadSecList.get(i) + ",";
+			}
+			es1 = es1.substring(0, es1.length()-1) + ")";
+			sql += " and s.r_id in "+es1;
+		}
+		System.out.println(sql);
 		Map<String, Object> geojson = CommUtils.getGeojson(jdbcTemplate, sql);
 		return geojson;
     }
