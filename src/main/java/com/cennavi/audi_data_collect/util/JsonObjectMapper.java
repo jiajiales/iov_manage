b62f1dc92e60315e7ddf6b67aecfcee0cc6b4174
xueTiger <601956149@qq.com>
2019-05-09 17:49:08 +0800

    xuehu commit

---
+++ src/main/java/com/cennavi/audi_data_collect/util/JsonObjectMapper.java
@@ -0,0 +1,31 @@
+package com.cennavi.audi_data_collect.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+/**
+ * ResponseBody处理null属性值
+ *继承处理类ObjectMapper
+ */
+public class JsonObjectMapper extends ObjectMapper {
+    private static final long serialVersionUID = 1L;
+
+    public JsonObjectMapper() {
+        super();
+        // 空值处理为空串
+        this.getSerializerProvider().setNullValueSerializer(
+                new JsonSerializer<Object>() {
+                    @Override
+                    public void serialize(Object value, JsonGenerator jg,
+                            SerializerProvider sp) throws IOException {
+                        jg.writeString("");
+                    }
+                });
+    }
+}
